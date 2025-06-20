d5c660ff27b10ea227de61a14f510aa668250a88
Abadear18 <abadearnassim00@gmail.com>
2025-06-20 20:40:55 +0300

    Add BankAccount class and CLI interaction script for task 0

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,31 @@
+import random
+
+def guess_the_number():
+    secret_number = random.randint(1, 100)
+    attempts = 0
+
+    print("Welcome to the Number Guessing Game!")
+    print("I have selected a number between 1 and 100. Try to guess it!")
+
+    while True:
+        try:
+            guess = int(input("Enter your guess: "))
+            attempts += 1
+
+            if guess < 1 or guess > 100:
+                print("Please enter a number between 1 and 100.")
+                continue
+
+            if guess < secret_number:
+                print("Too low! Try again.")
+            elif guess > secret_number:
+                print("Too high! Try again.")
+            else:
+                print(f"Congratulations! You guessed the number {secret_number} in {attempts} attempts.")
+                break
+
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
+
+# Run the game
+guess_the_number()
